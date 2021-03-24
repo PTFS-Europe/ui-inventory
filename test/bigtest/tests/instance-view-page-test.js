82fbdf69547a994e0368ed52166dada428a16704
Zak Burke <zburke@cornell.edu>
2021-03-24 13:55:27 -0400

    skip suspicious test

--- test/bigtest/tests/instance-view-page-test.js
+++ test/bigtest/tests/instance-view-page-test.js
@@ -36,7 +36,7 @@ const headersIndex = {
   MATERIAL_TYPE: 9,
 };
 
-describe('InstanceViewPage', () => {
+describe.skip('InstanceViewPage', () => {
   let instance;
 
   const visitingViewInventoryPageWithContent = () => {
