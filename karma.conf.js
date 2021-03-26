a1aa4d0d1d4de05536ae0fd0cfa4fc72284e6427
Zak Burke <zburke@cornell.edu>
2021-03-26 10:15:17 -0400

    puppeteer in path

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,6 @@
 /** @param {import('karma').Config} config */
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = config => config.set({
 
   client: {
@@ -8,17 +10,19 @@ module.exports = config => config.set({
     },
   },
   browserDisconnectTolerance: 10,
-  browserDisconnectTimeout: 10000,
-  browserNoActivityTimeout: 10000,
+//  browserDisconnectTimeout: 10000,
+//  browserNoActivityTimeout: 10000,
 
   flags: [
     '--disable-gpu',
-    '--no-sandbox',
+    '--no-sandbox'
+/*
     '--disable-background-timer-throttling',
     '--disable-renderer-backgrounding',
     '--disable-backgrounding-occluded-windows',
     '--sourcemaps=false',
     '--source-maps=false',
     '--js-flags=--max-old-space-size=8196'
+*/
   ],
 });
